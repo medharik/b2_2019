824fbc3e90dbfe9681f950552563dd33f3b165ea
unknown <med.harik@gmail.com>
2019-12-20 10:59:19 +0000

    bilan contact bootstrap js

---
+++ js/s3/valider.js
@@ -0,0 +1,11 @@
+function valider() {
+  var np = document.getElementById("np");
+  var np_error = document.getElementById("np_error");
+
+  if (np.value.length < 3) {
+    np_error.innerHTML =
+      "<i class='fa fa-thumbs-down'></i>nombre de caracteres minimal est 3";
+  } else {
+    np_error.innerHTML = "<i class='fa fa-thumbs-up'></i>";
+  }
+}
