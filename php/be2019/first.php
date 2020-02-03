2314ad8250c5e978511cef868005985d0e44cc3a
unknown <med.harik@gmail.com>
2020-02-03 13:03:22 +0000

    PHP , variables et conditions et affichage

--- php/be2019/first.php
+++ php/be2019/first.php
@@ -1,8 +1,26 @@
 <?php 
+
 $prenom="ALI";
-$age=28;
+$age=30;
 $taille=2;
+$hf="homme";
+if ($age>18) {
+    $m="Majeur";
+} else {
+$m="Mineur";
+}
+
+if($hf=="homme"){
+$couleur="#0F0";
+}
+if($hf=="femme"){
+    $couleur="#F00";
+
+}
+
+
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,10 +29,11 @@ $taille=2;
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
-<body>
+<body style="color:<?php echo $couleur;?>;">
     <h1>Bienvenue <?php echo $prenom;  ?></h1>
     <p>
     age est <?php echo $age; ?> , la taille est de : <?php echo $taille;?>
     </p>
+    <h4>Vous etes : <?php  echo $m;?></h4>
 </body>
 </html>
