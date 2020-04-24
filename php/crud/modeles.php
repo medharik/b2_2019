675ff3e7620cbe204363fb6f9f4dd55f23192d07
unknown <med.harik@gmail.com>
2020-04-24 16:23:06 +0000

    UPLOAD FILE

--- php/crud/modeles.php
+++ php/crud/modeles.php
@@ -1,8 +1,10 @@
+
 <?php
+
 // les fonctions CRUD
 // functions (sous programmes )  : 
 // connecter_db
-
+define ('MAX_SIZE',8*1024*1024);
 function connecter_db(){
 
     // silent_mode , warning_mode , exception_model
@@ -100,9 +102,38 @@ function modifier($libelle, $prix,$photo,$id){
     // televersement = upload d'un fichier
 // infos sur fichier : array( name=> 'a.png' , tmp_name  , size , error, type
     function uploader($infos){
-$name =$infos['name'];
-$tmp =$infos['tmp_name'];
-$chemin="images/$name";//images/a.png
-move_uploaded_file($tmp,$chemin);
-return $chemin;
+            $name =$infos['name'];
+            $tmp =$infos['tmp_name'];
+            $taille= filesize($tmp);// calcul la taille du fichier en octect
+            $file_info=  pathinfo($name);
+            $extension= strtolower( $file_info['extension']);//PNG=>png
+            $autorise=['png','jpg','jpeg','gif','bmp','ico'];
+            
+            $new_name=md5(time().random_int(0,9999999).$name);//a569fmdklsdjfkddsmddlsdkl
+
+            $chemin="images/$new_name.$extension";//images/a.png
+            
+//varification de la taille 
+  if($taille> MAX_SIZE){
+    //   header("location:create.php?err=size");
+    //   exit();
+    return "la taille du fichier ne doit pas depassee ".MAX_SIZE/(1024*1024).'Mo';
+}
+// verification dy type 
+else if(!in_array($extension,$autorise) ){
+    
+    return  "Ce n'est pas une image";
+           
+            //AUTRE probeleme  d'upload
+        }else if(!move_uploaded_file($tmp,$chemin)){
+           return "une erreur est survenue lors du chargement du fichier";
+        }else {
+
+
+
+    return $chemin;
+}
+
+
+         
     }
